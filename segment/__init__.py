202e9aa520ca4eaed8718c6d3723ace292d81d7f
Michael Pedersen <mp@beefy.dk>
2016-06-30 14:25:09 -0500

    Bump version number

--- segment/__init__.py
+++ segment/__init__.py
@@ -7,7 +7,7 @@ Easy integration of Segment analytics into Django projects.
 """
 
 __title__ = 'segment'
-__version__ = '0.0.1'
+__version__ = '0.0.2'
 __author__ = 'Michael Pedersen'
 __license__ = 'MIT'
 __copyright__ = 'Copyright 2016 SteelSeries'
